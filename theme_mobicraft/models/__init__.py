388296546d40df24c05cc2cb91ce52771bc3b837
tahir <tahir@india.biztechconsultacy.com>
2022-01-12 19:16:55 +0530

    Remove all Module

--- theme_mobicraft/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of AppJetty. See LICENSE file for full copyright and licensing details.
-
-from . import slider
-from . import products
-from . import website
-from . import theme
-from . import theme_custom
