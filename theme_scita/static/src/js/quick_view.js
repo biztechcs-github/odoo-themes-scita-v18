948ad40c288963ae3fe3119843e40e8cc5936971
Vaibhav Sathwara <vaibhav.js@mail.biztechcs.com>
2025-06-02 12:06:38 +0530

    WIP:Quick view

--- theme_scita/static/src/js/quick_view.js
+++ theme_scita/static/src/js/quick_view.js
@@ -19,6 +19,9 @@ odoo.define('theme_scita.quick_view',[], function(require) {
         events: {
             'click .quick_view_sct_btn': 'quickViewData',
             'click .cart_view_sct_btn': 'cartViewData',
+            "click .css_attribute_color": "_onMouseEnterSwatch",
+            // "click .css_attribute_color": "_onMouseLeave",
+            
         },
         quickViewData: function(ev) {
             var element = ev.currentTarget;
@@ -64,6 +67,38 @@ odoo.define('theme_scita.quick_view',[], function(require) {
                 return document.querySelector("#product_detail_main").dataset.image_layout;
             });
         },
+        _onMouseEnterSwatch: function (ev) {
+            const $swatch = $(ev.currentTarget);
+            const $product = $swatch.closest('#product_detail');
+            const $img = $product.find('img').first();      
+            console.log("Swatch mouse enter event triggered", $swatch, $img);
+            console.log($product,"<<<<<<<<<<<<<<<<<<< Product", );
+            
+                  
+            // this.image= $img;
+        
+            this.defaultSrc = $img.attr('data-default-img-src');      
+            console.log("Default image source:", this.defaultSrc);
+            const previewSrc = $swatch.find('label').data('previewImgSrc');
+            console.log("Preview image source:", previewSrc);
+
+            // if (previewSrc) {
+            //     this._updateImgSrc(previewSrc, $img);
+            //     $swatch.addClass("active");
+            // }
+        },
+        
+        // _onMouseLeave: function () {
+        //      this._updateImgSrc(this.defaultSrc,this.image);
+        // },
+        
+        // _updateImgSrc: function (src, $img) {        
+        //     if ($img && src) {
+        //         $img.attr('src', src);
+        //     } else {
+        //         console.warn("Image element or source is missing.");
+        //     }
+        // },
         cartViewData: function(ev) {
             var element = ev.currentTarget;
             var product_id = $(element).attr('data-id');
@@ -71,6 +106,6 @@ odoo.define('theme_scita.quick_view',[], function(require) {
                 $("#shop_cart_view_modal").html(data);
                 $("#shop_cart_view_modal").modal('show');
             });
-        }
+        },
     });
 });;
