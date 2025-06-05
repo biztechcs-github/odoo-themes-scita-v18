970cf6bc9ada522d0f8fab335854b204e03b88c2
krushita.mistry <krushita.m@biztechcs.com>
2025-06-05 16:46:31 +0530

    iamge hotspot add and snippet icon add

--- theme_scita/__manifest__.py
+++ theme_scita/__manifest__.py
@@ -129,7 +129,7 @@ AMP Support
         'views/theme_cusomization.xml',
         'views/snippets/snippets.xml',
         'views/image_hotspot_views.xml',
-        'views/image_hotspot_template.xml',
+        # 'views/image_hotspot_template.xml',
     ],
     'support': 'support@appjetty.com',
     'live_test_url': 'https://theme-scita-v17.appjetty.com/',
