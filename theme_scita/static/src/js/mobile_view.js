3fb9b93d4bbdb8d0cfef5d7824decbede3608f7e
krushita.mistry <krushita.m@biztechcs.com>
2025-06-06 14:22:39 +0530

    review design and changes

--- theme_scita/static/src/js/mobile_view.js
+++ theme_scita/static/src/js/mobile_view.js
@@ -3,13 +3,6 @@
 
 $(document).ready(function () {
     $('.ab-category').on("click",function(ev) {
-        var $link = $(ev.currentTarget);
-        if ($link.hasClass('active')) {
-            $link.removeClass('active');
-        } else {
-            $link.removeClass('active');
-            $link.addClass('active');
-        }
         $('body').addClass("sct_shop_box_overlay");
     });
     $('.ab-category-close').on("click",function() {
